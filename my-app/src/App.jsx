9919565927f479db73af96f3e160a8cb7cd17fd8
Vania397 <vania_397@abv.bg>
2023-12-05 22:29:33 +0200

    Update new/old files

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -17,8 +17,6 @@ import Edit from './components/edit/Edit.jsx';
 import Footer from "./components/footer/Footer.jsx";
 import Logout from './components/logout/Logout.jsx';
 import AuthGuards from './components/guards/AuthGuards.jsx';
-import AuthorizedCreateGalleryForm from './components/guards/AuthorizedCreateGalleryForm.jsx';
-
 
 
 
@@ -32,22 +30,24 @@ function App() {
 
 
         <main className="main">
-          <Routes>
-            <Route path={Path.Home} element={<Home />} />
-            <Route path="/details/:shipId" element={<ShipInfo />} />
-            <Route path="/create-gallery/:shipId" element={<AuthorizedCreateGalleryForm />} />
-            <Route path={Path.Catalog} element={<Catalog />} />
+        
+            <Routes>
+              <Route path={Path.Home} element={<Home />} />
+              <Route path="/details/:shipId" element={<ShipInfo />} />
+              <Route path={Path.Catalog} element={<Catalog />} />
 
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
 
-            <Route element={<AuthGuards />}>
-              <Route path={Path.Create} element={<Create />} />
-              <Route path='/edit/:shipId' element={<Edit />} />
-              <Route path={Path.Logout} element={<Logout />} />
-            </Route>
+              <Route element={<AuthGuards />}>
+                <Route path={Path.Create} element={<Create />} />
+                <Route path='/edit/:shipId' element={<Edit />} />
+                <Route path={Path.Logout} element={<Logout />} />
+              </Route>
 
-          </Routes>
+            </Routes>
+
+          
 
           <Footer />
         </main>
--- my-app/src/components/catalog/ShipItem.jsx
+++ my-app/src/components/catalog/ShipItem.jsx
@@ -1,11 +1,10 @@
 import React, { useContext, useState } from 'react'
 import { useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
-
 import AuthContext from '../../contexts/authContext.jsx';
-import * as shipService from '../../services/shipService.js'
 
 import Gallery from "./Gallery"
+import { FaHeart } from 'react-icons/fa';
 
 import "./catalog.css"
 
@@ -23,17 +22,11 @@ const ShipItem = ({
 }) => {
   const { userId } = useContext(AuthContext);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
-  const [latestShips, setLatestShips] = useState([]);
+  const [showLike, setShowLike] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const [showGallery, setShowGallery] = useState(false);
-  const shipId = _id;
-
-  useEffect(() => {
-    shipService.getLatest()
-      .then(result => setLatestShips(result));
-  }, [])
-
-
+  const [showFavWindows, setShowFavWindows] = useState(false);
   const infoClickHandler = () => {
     onInfoClick(_id);
 
@@ -42,13 +35,16 @@ const ShipItem = ({
     setShowDeleteModal(true);
     onDeleteClick(_id)
   };
+  const handleGalleryToggleClick = () => {
+    setLoading(true);
+    setShowGallery((prevShowGallery) => !prevShowGallery);
 
-  const handleShowGalleryClick = () => {
-    setShowGallery(true);
+    // Set a timeout to hide the loading message after 3000 milliseconds (3 seconds)
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
   };
-  const handleHideGalleryClick = () => {
-    setShowGallery(false);
-  };
+
 
   const isOwner = _ownerId === userId;
 
@@ -63,36 +59,38 @@ const ShipItem = ({
 
         <h3 className='line'>{cruiseLine}</h3>
         {/* <p>S.S. Joie de Vivre brings the “joy of living” to the rivers of France. Step aboard your floating pied-à-terre in France, featuring handcrafted furniture and fine antiques, original artwork, and...</p>
-        {/* <a role="button" className="btn btn-secondary" href="/us/ships/ss-joie-de-vivre">View the Ship</a>
-        <a role="button" className="btn btn-primary" href="/us/river-cruise?ships=ss-joie-de-vivre">View Cruises</a> */}
-        <p><strong>River/s:</strong> <em>{rivers}</em><span className="separator">/</span><strong>Guests:</strong> <em>128</em></p>
+        {/* <a role="button" className="btn btn-secondary" href="/us/s/ss-joie-de-vivre">View the </a>
+        <a role="button" className="btn btn-primary" href="/us/river-cruise?s=ss-joie-de-vivre">View Cruises</a> */}
+        {/* <p><strong>River/s:</strong> <em>{rivers}</em><span className="separator">/</span><strong>Guests:</strong> <em>128</em></p>
+        <button onClick={handleLikeToggle} className="like-button">
+          <FaHeart style={{ color: state.likes === 1 ? 'red' : 'black' }} />
+          {state.likes > 0 && <span className="like-count">{state.likes}</span>}
+          {state.likes === 1 ? 'Dislike' : 'Like'}
+        </button> */}
 
         <Button className="btn btn-secondary" variant="info" onClick={infoClickHandler}>Read  More</Button>
         {
           isOwner &&
           <Button className="btn btn-primary" variant="danger" onClick={handleDeleteClick}>Delete</Button>
         }
+        {/* {!isOwner && (
+          <Button onClick={handleAddToFavourites} disabled={state.favourites.includes(Id)}>
+            {state.favourites.includes(Id) ? 'Added to Favourite' : 'Add to Favourite'}
+          </Button>
+        )} */}
 
         <Button
-          onClick={handleShowGalleryClick}
-          variant="primary"
+          onClick={handleGalleryToggleClick}
+          variant={showGallery ? "danger" : "primary"}
           style={{ padding: '5px 40px', margin: '20px' }}
         >
-          Show Gallery
+          {showGallery ? "Hide Gallery" : "Show Gallery"}
         </Button>
-        {showGallery && (
-          <Button
-            onClick={handleHideGalleryClick}
-            variant="danger"
-            style={{ padding: '5px 40px', margin: '20px' }}
-          >
-            Hide Gallery
-          </Button>
-        )}
+
 
         {/* Conditionally render the Gallery component */}
         {showGallery && (
-          <Gallery shipId={shipId} ownerId={_ownerId} shipName={name} />
+          <Gallery shipId={_id} />
         )}
       </div>
     </div>
